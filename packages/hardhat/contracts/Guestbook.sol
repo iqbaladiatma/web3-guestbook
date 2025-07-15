cfe9c2f5642d75785a53a009a61a2f1d1d28f449
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2025-07-15 07:33:18 +0700

    smart

--- packages/hardhat/contracts/Guestbook.sol
+++ packages/hardhat/contracts/Guestbook.sol
@@ -15,7 +15,7 @@ contract Guestbook {
     function postMessage(string calldata _message) public {
         require(bytes(_message).length > 0, "Message cannot be empty");
         messages.push(Message(msg.sender, _message, block.timestamp));
-        emit NewMessage(msg.sender, _message, block.timestamp);
+        emit NewMessage(msg.sender, block.timestamp, _message);
     }
 
     function getAllMessages() public view returns (Message[] memory) {
